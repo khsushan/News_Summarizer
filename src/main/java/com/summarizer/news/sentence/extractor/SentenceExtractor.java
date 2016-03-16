1762bb0af9e750eaac3688d68c5423b4d563f3fc
khsushan <khsushan@gmail.com>
2016-03-16 18:06:56 +0530

    git add sentence extractor class

--- src/main/java/com/summarizer/news/sentence/extractor/SentenceExtractor.java
+++ src/main/java/com/summarizer/news/sentence/extractor/SentenceExtractor.java
@@ -1,7 +1,25 @@
 package com.summarizer.news.sentence.extractor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ushan on 3/5/16.
  */
 public class SentenceExtractor {
+    private List documents = new ArrayList<String[]>();
+    private List allWords = new ArrayList<String>(); //to hold all terms
+
+    public  String[] getExtractedWordInGivenDocument(StringBuilder builder){
+        String[] tokenizedTerms = builder.toString().replaceAll("[\\W&&[^\\s]]", "").split("\\W+");   //to get individual terms
+        for (String term : tokenizedTerms) {
+            if (!allWords.contains(term)) {  //avoid duplicate entry
+                allWords.add(term);
+            }
+        }
+        documents.add(tokenizedTerms);
+        return tokenizedTerms;
+    }
+
+
 }
